f353d66c69d30e0a32334d433433acf2486bd3ee
john <john@langcore.cn>
2024-09-06 10:46:48 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,61 +1,81 @@
 'use client';
-import { Button, Input, Spacer } from '@nextui-org/react';
-import { useForm, type SubmitHandler } from 'react-hook-form';
-type LoginFormInput = {
-  name: string;
-  password: string;
+import { Button, Input, Select, SelectItem, Spacer } from '@nextui-org/react';
+import { set, useForm, type SubmitHandler } from 'react-hook-form';
+export const animals = [
+  { key: 'cat', label: 'Cat' },
+  { key: 'dog', label: 'Dog' },
+  { key: 'elephant', label: 'Elephant' },
+  { key: 'lion', label: 'Lion' },
+  { key: 'tiger', label: 'Tiger' },
+  { key: 'giraffe', label: 'Giraffe' },
+  { key: 'dolphin', label: 'Dolphin' },
+  { key: 'penguin', label: 'Penguin' },
+  { key: 'zebra', label: 'Zebra' },
+  { key: 'shark', label: 'Shark' },
+  { key: 'whale', label: 'Whale' },
+  { key: 'otter', label: 'Otter' },
+  { key: 'crocodile', label: 'Crocodile' },
+];
+type FormInput = {
+  animal: string;
 };
 export default function Home() {
   const {
     register,
-    handleSubmit,
-    formState: { errors },
-    setValue,
-    getValues,
-  } = useForm<LoginFormInput>();
+    watch,
+  } = useForm<FormInput>();
+  const { register: register2, watch: watch2, setValue: setValue2 } = useForm<FormInput>();
   return (
     <>
-      <p>1. Press button to set the name to root</p>
-      <p>2. The username input is set to root</p>
-      <p>3. Move the mouse over the input box</p>
-      <p>4. The value in the input box disappeared</p>
-      <Spacer y={4} />
-
-      <Button
-        onClick={() => {
-          setValue('name', 'root');
-        }}
-      >
-        Press me to set the name to root
-      </Button>
-      <Spacer y={4} />
-
-      <div className="w-64">
-        <Input
-          color={errors.name ? 'danger' : 'default'}
-          label="Username"
-          variant="bordered"
-          type="text"
-          placeholder="Please enter your name"
-          tabIndex={-1}
-          {...register('name', {
-            required: 'This is a required field',
-          })}
-          description={errors.name?.message}
-        />
-        <Spacer y={4} />
-        <Input
-          color={errors.password ? 'danger' : 'default'}
-          label="Password"
-          variant="bordered"
-          type="password"
-          placeholder="Please enter your password"
-          tabIndex={-1}
-          {...register('password', {
-            required: 'This is a required field',
-          })}
-          description={errors.password?.message}
-        />
+      <div className="p-8">
+        <h1>1. The return value of the Select component: </h1>
+        <div>
+          <Select
+            label="Select an animal"
+            className="max-w-xs"
+            selectionMode="multiple"
+            {...register('animal', {
+              required: 'This is required',
+            })}
+          >
+            {animals.map((animal) => (
+              <SelectItem key={animal.key}>{animal.label}</SelectItem>
+            ))}
+          </Select>
+          <Spacer y={4} />
+          <p className="font-bold">The value comes from useForm's watch.</p>
+          <p>animal value: {`${watch('animal')}`}</p>
+          <p>animal type: {typeof watch('animal') ?? ''}</p>
+        </div>
+        <br />
+        <br />
+        <br />
+        <h1>2. Set the value to the Select component: </h1>
+        <div>
+          <Select
+            label="Select an animal"
+            className="max-w-xs"
+            selectionMode="multiple"
+            {...register2('animal', {
+              required: 'This is required',
+            })}
+          >
+            {animals.map((animal) => (
+              <SelectItem key={animal.key}>{animal.label}</SelectItem>
+            ))}
+          </Select>
+          <Spacer y={4} />
+          <p>animal value: {`${watch2('animal')}`}</p>
+          <p>animal type: {typeof watch2('animal') ?? ''}</p>
+          <Button
+            onClick={() => {
+              setValue2('animal', 'Dog,Cat');
+            }}
+          >
+            Set "Dog,Cat"
+          </Button>
+          <p>The Select component is not set to the corresponding state.</p>
+        </div>
       </div>
     </>
   );
